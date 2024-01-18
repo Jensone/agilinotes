068c606f6a96ebd79b4ee122acd1152301714743
Jensone <hello@jensone.com>
2024-01-19 00:52:08 +0100

    🔧 corriger(security.yaml) : ajouter un authentificateur personnalisé et une route de déconnexion 🔧 corriger(User.php) : ajouter une contrainte d'unicité sur l'email et ajouter une propriété isVerified 🔧 corriger(User.php) : ajouter des méthodes pour vérifier si l'utilisateur est vérifié et pour définir la vérification 🔧 corriger(UserAuthenticator.php) : ajouter un authentificateur personnalisé pour gérer l'authentification de l'utilisateur 🔧 corriger(confirmation_email.html.twig) : ajouter un modèle pour l'email de confirmation d'inscription 🔧 corriger(login.html.twig) : ajouter un modèle pour la page de connexion

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,11 +8,13 @@ use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -56,6 +58,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $link = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -245,6 +250,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setLink(?string $link): static
     {
         $this->link = $link;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
